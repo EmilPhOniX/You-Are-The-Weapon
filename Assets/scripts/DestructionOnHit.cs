9b0c28ec6188b22283c229388fb97b8d07a103d5
Emilien <emilien.derobert@gmail.com>
2025-06-02 23:42:58 +0200

    zombies dying

--- Assets/scripts/DestructionOnHit.cs
+++ Assets/scripts/DestructionOnHit.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class DestructionOnHit : MonoBehaviour
 {
+    public string targetTag = "bullet";
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Bullet"))
+        if (other.CompareTag(targetTag))
         {
             Destroy(gameObject);
         }
